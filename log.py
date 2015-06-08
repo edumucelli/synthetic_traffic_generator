2deb5d88770697757a14606b3d04d80d165aff7c
Eduardo Mucelli Rezende Oliveira <edumucelli@gmail.com>
2015-06-08 16:46:07 +0200

    Commiting the basic code

---
+++ log.py
@@ -0,0 +1,13 @@
+import logging
+from os import path
+
+logger = logging.getLogger('Progress')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler(path.abspath(path.join(".", "progress.log")))
+formatter = logging.Formatter('%(asctime)s %(message)s', datefmt="%d-%m-%Y %H:%M:%S")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+def debug(message):
+  logger.debug(message)
---
+++ run.py
@@ -0,0 +1,12 @@
+import generator
+import sys
+from os import path
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    number_of_synthetic_users = int(sys.argv[1])
+  else:
+    number_of_synthetic_users = generator.NUMBER_OF_SYNTHETIC_USERS
+  print "[+] Generating %d synthetic users and their respective hourly traffic" % number_of_synthetic_users
+  print "[+] Per-user synthetic traffic will be stored in '%s'" % path.join('.', generator.USERS_DIRECTORY, generator.SYNTHETIC_DIRECTORY)
+  generator.generate_synthethic_users_and_traffic(number_of_synthetic_users)
